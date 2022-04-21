5e23c72c1aad4acb324a10365a9b8d096740d24c
TrakaDev <trakadev@gmail.com>
2022-04-21 08:12:14 -0500

    [ADD] component btn

---
+++ src/components/DelegateButton.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import * as WASM_lib from "@emurgo/cardano-serialization-lib-browser";
+import CardanoWalletsApi, { findWallet } from "../utils/cardano-wallets-api";
+import "./index.css";
+
+export const DelegateButton = ( { btnText, poolId, tagTx, blockfrostKey, walletName, callBackFunc = null} ) => {    
+    const [isLoading, setIsLoading] = useState(false)
+
+    const handleCustomAPI = async () => {     
+        try {
+          setIsLoading(true);
+          const wallet_obj = window.cardano;
+          if (wallet_obj) {
+            const compatible = await findWallet(walletName, wallet_obj);
+            const wallet = await CardanoWalletsApi(
+              compatible,
+              blockfrostKey,
+              WASM_lib
+            );         
+            const resp = await wallet.delegate({
+              poolId: poolId,
+              metadata: tagTx,
+              metadataLabel: "721",
+            });
+            // tx hash
+            console.dir(resp);
+            //Exito
+          } else {
+            console.log("no wallet detected");
+            // NO WALLET          
+          }
+        } catch (error) {
+          // ERROR
+         console.log(error) 
+        }
+        setIsLoading(false);        
+    };
+
+    return (
+        <>          
+          <button type="button" onClick={handleCustomAPI}>
+            {btnText}
+          </button>          
+          {isLoading ? (
+            <div className="pos-center" id="loader_dapp">
+              <div className="loader" />
+              <h3 style={{text_align:"center"}}> Building Transaction </h3>
+            </div>
+          ) : null}
+        </>
+      )    
+}
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,58 +1,24 @@
-import React, { useState } from "react";
-import * as WASM_lib from "@emurgo/cardano-serialization-lib-browser";
+import React from "react";
 import { variables } from "../vars";
-import CardanoWalletsApi, { findWallet } from "../utils/cardano-wallets-api";
+import { DelegateButton } from "./delegateButton";
 import "./index.css";
-
+/*
+NOTES 
+Edit .htaccess to support wasm mime type
+*/
 const App = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  //const POOL_ID = "pool1adur9jcn0dkjpm3v8ayf94yn3fe5xfk2rqfz7rfpuh6cw6evd7w";
-  const POOL_ID = "pool15w7lejtrpk4868drc8h65cgmm504qlnzhh52e2yndgrs5a9w232"
-  const handleCustomAPI = async (walletName) => {     
-      try {
-        setIsLoading(true);
-        const wallet_obj = window.cardano;
-        if (wallet_obj) {
-          const compatible = await findWallet(walletName, wallet_obj);
-          const wallet = await CardanoWalletsApi(
-            compatible,
-            variables.blockfrost_testnet,
-            WASM_lib
-          );         
-          const resp = await wallet.delegate({
-            poolId: POOL_ID,
-            metadata: "tr4k4",
-            metadataLabel: "721",
-          });
-          // tx hash
-          console.dir(resp);
-          //Exito
-        } else {
-          console.log("no wallet detected");
-          // NO WALLET          
-        }
-      } catch (error) {
-        // ERROR
-       console.log(error) 
-      }
-      setIsLoading(false);        
-  };
+  const POOL_ID = "pool1adur9jcn0dkjpm3v8ayf94yn3fe5xfk2rqfz7rfpuh6cw6evd7w";
+  //const POOL_ID = "pool15w7lejtrpk4868drc8h65cgmm504qlnzhh52e2yndgrs5a9w232";
 
   return (
     <div>
-      <h1>Traka Was Here!</h1>
-      <button type="button" onClick={(e) => handleCustomAPI("nami")}>
-        NAMI Delegation
-      </button>
-      <button type="button" onClick={(e) => handleCustomAPI("eternl")}>
-        ETERNL D2elegation
-      </button>
-      {isLoading ? (
-        <div className="pos-center" id="loader_dapp">
-          <div className="loader" />
-          <h3 style={{text_align:"center"}}> Building Transaction </h3>
-        </div>
-      ) : null}
+      <DelegateButton
+        btnText={"Delegar ccvault"}
+        poolId={POOL_ID}
+        tagTx={"Tr4k4"}
+        blockfrostKey={variables.blockfrost_testnet}
+        walletName={"eternl"}
+      />
     </div>
   );
 };
